eefe7a01b2447a28c0b1d1758d3d773cb3a125c1
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2023-12-24 21:02:53 +0700

    feat: day 16 part 1
    Change-Id: I91c61f329dfb57b62c55e46439f52b04e66e5b7d

---
+++ 16/part_1.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+enum DIRECTION {
+    LEFT = 1,
+    TOP = 2,
+    RIGHT = 3,
+    BOTTOM = 4,
+};
+
+struct point {
+    long long x;
+    long long y;
+    DIRECTION d;
+};
+
+void printMatrix(const std::vector<std::vector<char> >& matrix) {
+    for (const auto& row : matrix) {
+        for (const char& ch : row) {
+            std::cout << ch << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+std::string getKey(point p) {
+    return std::to_string(p.x) + "#" + std::to_string(p.y);
+}
+
+int main() {
+    std::ifstream file("input.txt");
+
+    if (file.is_open()) {
+
+        long long total = 0;
+        std::string line;
+        std::vector<std::vector<char> > world; // extended row
+        std::vector<std::vector<char> > world2; // extended row
+
+
+
+        while (std::getline(file, line)) {
+            std::vector<char> row;
+            std::vector<char> row2;
+            for (int i = 0; i<line.size();i++) {
+                row.push_back(line[i]);
+                row2.push_back(line[i]);
+            }
+            world.push_back(row);
+            world2.push_back(row2);
+        }
+
+        int n = world.size();
+        int m = world[0].size();
+
+        std::cout<<"n: "<<n<<", m: "<<m<<"\n";
+
+        std::vector<point> queue;
+        std::unordered_map<std::string, bool> visited;
+
+        queue.push_back({0,0,RIGHT});
+        printMatrix(world2);
+        while (queue.size() >0 ) {
+            std::vector<point> newQueue;
+
+            for (int i=0;i<queue.size(); i++) {
+                point currPoint = queue[i];
+//                std::cout<<"x: "<<currPoint.x<<", y: "<<currPoint.y<<", dir: "<<currPoint.d<<"\n";
+                if (currPoint.x < 0 || currPoint.x >= n || currPoint.y < 0 || currPoint.y >= m) {
+//                    std::cout<<"[skipped] x: "<<currPoint.x<<", y: "<<currPoint.y<<", dir: "<<currPoint.d<<"\n";
+                    continue;
+                }
+//                std::cout<<"world: "<<world[currPoint.x][currPoint.y]<<std::endl;
+                world2[currPoint.x][currPoint.y] = '#';
+//                printMatrix(world2);
+
+                std::string key = getKey(currPoint);
+                if (world[currPoint.x][currPoint.y] == '.') {
+                    int dx = 0;
+                    int dy = 0;
+                    if (currPoint.d == LEFT) {
+                        dy = -1;
+                    } else if (currPoint.d == RIGHT) {
+                        dy = 1;
+                    } else if (currPoint.d == TOP) {
+                        dx = -1;
+                    } else if (currPoint.d == BOTTOM) {
+                        dx = 1;
+                    }
+
+                    newQueue.push_back({currPoint.x + dx, currPoint.y + dy, currPoint.d});
+                    visited[key] = true;
+                } else {
+                    if (world[currPoint.x][currPoint.y] == '|' && !visited[key]) {
+                        newQueue.push_back({currPoint.x-1, currPoint.y, TOP});
+                        newQueue.push_back({currPoint.x+1, currPoint.y, BOTTOM});
+                    } else if (world[currPoint.x][currPoint.y] == '-' && !visited[key]) {
+                        newQueue.push_back({currPoint.x, currPoint.y-1, LEFT});
+                        newQueue.push_back({currPoint.x, currPoint.y+1, RIGHT});
+                    } else if (world[currPoint.x][currPoint.y] == '/') {
+                        int dx = 0;
+                        int dy = 0;
+                        DIRECTION d = RIGHT;
+
+                        if (currPoint.d == RIGHT) {
+                            dx = -1;
+                            d = TOP;
+                        } else if (currPoint.d == TOP) {
+                            dy = 1;
+                            d = RIGHT;
+                        } else if (currPoint.d == LEFT) {
+                            dx = 1;
+                            d = BOTTOM;
+                        } else if (currPoint.d == BOTTOM){
+                            dy = -1;
+                            d = LEFT;
+                        }
+                        newQueue.push_back({currPoint.x + dx, currPoint.y + dy, d});
+                    } else if (world[currPoint.x][currPoint.y] == '\\') {
+                        int dx = 0;
+                        int dy = 0;
+                        DIRECTION d = RIGHT;
+
+                        if (currPoint.d == RIGHT) {
+                            dx = 1;
+                            d = BOTTOM;
+                        } else if (currPoint.d == TOP) {
+                            dy = -1;
+                            d = LEFT;
+                        } else if (currPoint.d == LEFT) {
+                            dx = -1;
+                            d = TOP;
+                        } else if (currPoint.d == BOTTOM){
+                            dy = 1;
+                            d = RIGHT;
+                        }
+                        newQueue.push_back({currPoint.x + dx, currPoint.y + dy, d});
+                    }
+                    visited[key] = true;
+                }
+            }
+            queue = newQueue;
+        }
+
+
+        total = visited.size();
+
+        file.close();
+
+        std::cout << total << std::endl;
+
+    } else {
+        std::cout << "Failed to open file";
+        return -1;
+    }
+
+    return 0;
+}
